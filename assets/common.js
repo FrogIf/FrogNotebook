35423d02db4ddd0beb8ffd81416a6a9cf581a62d
frogif <skyfrog@163.com>
2022-01-14 10:51:30 +0800

    根路径动态指定

--- assets/common.js
+++ assets/common.js
@@ -1,6 +1,10 @@
 $(function(){
     document.title = "FrogNotebook";
     if($("#home").length > 0){
-        $("#home").attr("href", "/FrogNotebook/");
+        $("#home").attr("href", sessionStorage.getItem("rootPath"));
     }
 })
+
+function refreshRootPath(){
+    sessionStorage.setItem("rootPath", location.pathname);
+}
